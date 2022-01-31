bdce79fa5d3d4348f8175191111a5a4a6471df9f
Michael Ding <yandy.ding@gmail.com>
2022-02-01 01:33:03 +0800

    Init Commit

---
+++ include/sdnova_simulation/sdnova_ctrl.hpp
@@ -0,0 +1,19 @@
+#ifndef SDNOVA_SIMULATION__SDNOVA_CTRL_HPP_
+#define SDNOVA_SIMULATION__SDNOVA_CTRL_HPP_
+
+#include "sdnova_simulation/visibility_control.h"
+
+namespace sdnova_simulation
+{
+
+class SdnovaCtrl
+{
+public:
+  SdnovaCtrl();
+
+  virtual ~SdnovaCtrl();
+};
+
+}  // namespace sdnova_simulation
+
+#endif  // SDNOVA_SIMULATION__SDNOVA_CTRL_HPP_
