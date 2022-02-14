dc347d13dcf2e939cf6c0452a5da0ff3464a775c
Michael Ding <yandy.ding@gmail.com>
2022-02-14 17:40:22 +0800

    update world

--- README.md
+++ README.md
@@ -1,9 +1,8 @@
 # SdNova Robot Package
 
-- rewrite world
+- 学习turtlebot3 调整项目结构
 - 根据urdf修正 sdquadx option model 相关逻辑 && 从urdf 赋值 option
 - 集成 nav2
   - 增加rviz
   - 增加lidar
   - 增加odm
-- 学习turtlebot3 调整项目结构
---
+++ launch/outdoor.py
@@ -0,0 +1,18 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.substitutions import ThisLaunchFileDir
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+
+def generate_launch_description():
+    pkg_sdnova = get_package_share_directory('sdnova_simulation')
+    world_path = os.path.join(pkg_sdnova, 'world/outdoor.sdf')
+    return LaunchDescription([
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource([ThisLaunchFileDir(), '/robot.py']),
+            launch_arguments={'world': world_path}.items()
+        ),
+    ])
